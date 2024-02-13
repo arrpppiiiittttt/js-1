2def774b7dcacc55615dab3eb6c528c61b328e5d
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-02-13 10:45:10 +0000

    conversionofdatatype

---
+++ 01_basics/03_conversionsandoperations.js
@@ -0,0 +1,36 @@
+let score ="33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+
+
+let scoree =null
+let valueInNumber1 = Number(scoree)
+console.log(typeof valueInNumber1);
+
+//"33" =>33
+//"33abc" =>NaN
+//true => 1
+//false =>0
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//1 => true
+//0 => false
+//""=> false
+//"Arpit"=> true
+
+
+let someNumber = 15
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
