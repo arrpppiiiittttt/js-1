74db01724ebf672cf31a23ef22b2685725a7b455
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-02-16 06:18:52 +0000

    03_js operations

--- 01_basics/03_conversionsandoperations.js
+++ 01_basics/03_conversionsandoperations.js
@@ -34,3 +34,39 @@ let someNumber = 15
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof(stringNumber));
+
+
+
+//***********************operations*******************//
+
+let value =3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2%2);
+console.log(2/2);
+
+let str1 ="hello"
+let str2 ="Arpit"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+/*console.log(true);
+console.log(+true);
+console.log(true+);
+console.log(+"");*/
+
+
+let gamecounter =100
+gamecounter++
+console.log(gamecounter);
