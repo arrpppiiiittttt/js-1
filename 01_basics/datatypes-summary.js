6d9b10d2782081e4a2e45b14ea83baf8b645d514
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-02-19 14:26:32 +0000

    summary-datatypes.js

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+// primitive datatypes
+/* 7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt */
+
+//Non-primitive (Reference)
+/* Arrays, Objects, Functions */
+
+/*arrrays => 
+const heros = ["Ironman","Spiderman","Hulk"]
+*/
+
+/* objects=>
+let obj={
+    name:"Arpit"
+    age:"19"
+}
+*/
+
+/* function=>
+const myFunction = function(){
+    console.log("Hello World")
+}
+*/
