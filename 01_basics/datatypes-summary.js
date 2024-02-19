c881facfc8fe982d3bb0ee18efbbaadf50201f96
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-02-19 14:52:54 +0000

    datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -20,3 +20,25 @@ const myFunction = function(){
     console.log("Hello World")
 }
 */
+
+// stack => primitive 
+// Heap => non-primitive
+
+let myname = "arpit"
+
+let anothername =myname
+anothername ="appu"
+
+console.log(myname);
+console.log(anothername);
+
+let userOne = {
+    email:"user@gmail.com"
+
+}
+
+let usertwo = userOne
+
+usertwo.email = "appu@gmail.com"
+console.log(userOne.email);
+console.log(usertwo.email);
