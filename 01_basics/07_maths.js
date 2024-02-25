5d58d599824e6c1c4acd11595cd73ae756275e24
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-02-25 13:04:06 +0000

    numbers.js

---
+++ 01_basics/07_maths.js
@@ -0,0 +1,15 @@
+console.log(Math);
+console.log(Math.abs(4));
+console.log(Math.abs(4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.7))
+console.log(Math.floor(4.7));
+console.log(Math.min(2,13,24,7));
+console.log(Math.max(2,13,24,7));
+console.log(Math.random());//here value always lies between 0 and 1
+console.log((Math.random()*10)+1);// here will always be +ve
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min - 1))+ min)
