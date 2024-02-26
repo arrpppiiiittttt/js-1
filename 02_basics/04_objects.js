f2db5fe6dc9da7b73ed65f282eef32f8146daba1
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-02-26 14:48:15 +0000

    04objects.js

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,45 @@
+//const tinderuser = new Object()
+const tinderuser = {}
+
+tinderuser.id = "123abc"
+tinderuser.name = "Sunny"
+tinderuser.loggedIn = false
+
+//console.log(tinderuser);
+
+const regularuser = {
+    email: "aaaaaaa@gmail.com",
+    fullname: {
+        username:{
+            firstname:"Arpit",
+            lastname:"Dhiman"
+        }
+    }
+}
+//console.log(regularuser.fullname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+
+//const obj3 = {obj1,obj2}
+//const obj3 = object.assign({},obj1,obj2)/////// here {} is used as target code and rest elements are treated as source
+const obj3 = {...obj1,...obj2} //maximumly used syntax
+//console.log(obj3);
+
+//when data comes from database, it is in the form of array of objects
+const users = [
+    {
+
+    },
+    {
+
+    },
+]
+
+console.log(tinderuser);
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('isLoggedIn'));
+console.log(tinderuser.hasOwnProperty('isLogged'));
