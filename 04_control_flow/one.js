b33e2bea574b2d7475e639ea169b47caffeaf8df
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-03-11 18:34:45 +0000

    one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,31 @@
+// if
+/*
+if(false){
+
+}
+// if true then execute otherwise false and code does not execute//
+
+//<,>,<=,>=,==,!=,===
+
+const temperature = 41
+
+if(temperature<50){
+    console.log("less than 50");
+}
+else{
+console.log("temperature is greater than 50");
+}
+
+console.log("Execute");
+*/
+
+// scope example
+/*
+const score = 200
+
+if(score>100){
+    const power = "Fly"
+    console.log(`user power:${power}`);
+}
+console.log(`user power:${power}`);// outside of scope
+*/
