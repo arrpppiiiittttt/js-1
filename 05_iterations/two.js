fb0a534cb9082fb8e9beb8090ea67dc16ad216de
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-03-12 18:01:55 +0000

    while and do while js

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+/*
+let index = 0
+while (index <=10) {
+    console.log(`value of index is ${index}`);
+    index = index +2
+}
+*/
+
+/*
+let myarray = ["Flash","Batman","Superman"]
+let arr =0
+while ( arr < myarray.length){
+    console.log(`value is ${myarray[arr]}`);
+    arr = arr +1
+}
+*/
+
+
+////////////////////////////////////Do While //////////////////////////////////////
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
