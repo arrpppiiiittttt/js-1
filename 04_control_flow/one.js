ba9d78f5939165f3cfa0bbf134901d6250479757
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-03-12 14:27:41 +0000

    one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -29,3 +29,28 @@ if(score>100){
 }
 console.log(`user power:${power}`);// outside of scope
 */
+/*
+const balance = 1000
+
+if (balance<500){
+    console.log("Less than 500");    
+} else if (balance<750){
+    console.log("Less than 750");
+}else if (balance<900){
+    console.log("Less than 900");
+}else{
+    console.log("Less than 1200");
+}
+*/
+
+const userLoggedIn = true
+const debitCard = true
+const LoggedInFromGoogle = false
+const LoggedInFromEmail = true
+
+if (userLoggedIn && debitCard){     // if any of the condition is false then result is not generated
+    console.log("Allow to buy Course");
+}
+if (LoggedInFromGoogle || LoggedInFromEmail){    // for checking multiple conditions
+    console.log("User Logged In");
+}
