9a8b93f2390cc004a5837d31c8e3e5d3da7cdb7b
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-03-13 17:41:19 +0000

    four.js

---
+++ 05_iterations/four.js
@@ -0,0 +1,19 @@
+const myObject = {
+    js: "javascript",
+    cpp: "C++",
+    rb: "Ruby",
+    swift: "Swift By Apple"
+}
+
+for (const key in myObject) {
+    //console.log(key);
+    //console.log(myObject[key]);
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+        
+    }
+
+const programming = ["js","rb","python","java","cpp"]
+for (const key in programming) {
+   //console.log(key);
+   //console.log(programming[key]);
+}
