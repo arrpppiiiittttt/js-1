54c37a2f26b50793d3cdaa19c74ab01d71ad3e70
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-03-14 14:52:02 +0000

    eight.js

---
+++ 05_iterations/eight.js
@@ -0,0 +1,9 @@
+//The reduce() method executes a reducer function for array element.
+
+//The reduce() method returns a single value: the function's accumulated result.
+
+//The reduce() method does not execute the function for empty array elements.
+
+//The reduce() method does not change the original array.
+const mynums = [1,2,3]
+const mytotal = mynums.reduce(function(accumulator,currentvalue))
