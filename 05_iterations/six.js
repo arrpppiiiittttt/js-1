a62b93363f2932ecf510212bf262e5d73ea0ab24
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-03-14 14:48:22 +0000

    six.js

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -7,7 +7,18 @@ const values = coding.forEach((item) => {
 })
 console.log(values);
 */
-
+/*
 const mynums = [1,2,3,4,5,6,7,8,9,10]
-const newnums = mynums.filter((num) => num>4 )
+const newnums = mynums.filter((num) => num>4 )  // if we use curly braces in filter function , then we have to use return keyword
 console.log(newnums);
+*/
+/*
+const newnums = []
+myNums.forEach((num) => {
+    if (num>4){
+        newnums.push(num)
+    }
+});
+console.log(newnums);
+*/
+
