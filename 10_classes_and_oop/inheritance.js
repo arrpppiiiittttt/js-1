7c52c5e0771653a0a4a1dd004ecaab9e35118a36
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-04-24 11:33:35 +0000

    inheritence.js

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,20 @@
+class User{
+    constructor(username){
+        this.username = username
+    }
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+class Teacher extends User{
+    constructor(username,email,password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+    addcourse(){
+        console.log(`A new course was added by${this.username}`);
+    }
+}
+const chai = new Teacher("chai","chai@email.com","123")
+chai.addcourse()
